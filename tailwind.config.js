642d3479126dc7011a7d5a5ae2a07b85ec15572a
cesarmouradev <cesarmouradeveloper@gmail.com>
2024-12-13 11:41:47 -0300

    Feat: Elementos globais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"], 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF0054", 
+        secondary: "#5800FF", 
+        accent: "#00D1FF", 
+        background: "#1F1B33", 
+        text: "#FFFFFF",
+      },
+      boxShadow: {
+        neon: "0 0 10px #FF0054, 0 0 20px #5800FF", 
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Adicionando Poppins como a fonte padrão
+      },
+    },
   },
   plugins: [],
 }
-
